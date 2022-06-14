393a4b411c3a2c7fe1869857935301d58aac5ccc
Therion95 <piotrpawelczyk95@gmail.com>
2022-06-15 01:48:23 +0200

    Done

---
+++ PredicaTask.DataManipulation/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using PredicaTask.Application.RepositoryInterfaces;
+using PredicaTask.Data;
+using PredicaTask.DataManipulation.Repositories;
+
+namespace PredicaTask.DataManipulation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly PredicaTaskDbContext _context;
+
+        public UnitOfWork(PredicaTaskDbContext context)
+        {
+            _context = context;
+        }
+
+        public IUserRepository Users => new UserRepository(_context);
+        public IWeatherRepository Weathers => new WeatherRepository(_context);
+        
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
